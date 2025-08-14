f97eabd484c0bba4c2afdd955eb3c7b31fcee0eb
Arturo Roncal <roncalhermenegildo@gmail.com>
2025-08-13 23:27:35 -0500

    Initial commit

---
+++ src/test/java/com/nttdata/customersService/CustomersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.customersService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
